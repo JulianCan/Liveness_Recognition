95e22f05a29edb08b4e8ef23b438adfc9b9cbe49
JulianC <julianscanongcbs@gmail.com>
2025-10-12 09:05:47 -0500

    vamos

--- BACK/server.js
+++ BACK/server.js
@@ -20,7 +20,7 @@ const answersRoutes = require('./routes/answers');
 const mediaRoutes = require('./routes/media');
 
 // Load environment variables
-dotenv.config();
+//dotenv.config();
 
 const app = express();
 
